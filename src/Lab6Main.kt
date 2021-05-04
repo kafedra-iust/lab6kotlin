9928c4b923f40873e2a9e61036d8be909822614e
kafedra-iust <kafedra.iust@gmail.com>
2021-05-04 12:58:09 +0300

    theory about 2D arrays TODO Strings

--- src/Lab6Main.kt
+++ src/Lab6Main.kt
@@ -1,4 +1,21 @@
 // Simple program template
 fun main() {
     // write your code here ;-)
+    // Створюємо двовимірний масив
+    val n = 5
+    val matrix = Array(n){ row -> IntArray(n) { col -> if (row==col) 1 else 0 } }
+
+//    for (i in matrix.indices) {
+//        for (j in matrix[i].indices) {
+//            matrix[i][j] = 1
+//        }
+//    }
+
+// виводимо дані масиву
+    for (array in matrix) {
+        for (value in array) {
+            print("$value ")
+        }
+        println()
+    }
 }
